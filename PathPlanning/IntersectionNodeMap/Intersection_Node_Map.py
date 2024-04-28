d0f7d891129082e09eb26634b41318abb0294371
tplaysted <t.playsted@gmail.com>
2024-04-28 18:58:56 +1000

    Pathfinding for the INM

--- PathPlanning/IntersectionNodeMap/Intersection_Node_Map.py
+++ PathPlanning/IntersectionNodeMap/Intersection_Node_Map.py
@@ -34,12 +34,17 @@ class NodeMap(ABC):
     The node map for a given environment. The map is internally represented as a digraph. 
 
     DG: The internal digraph
+    nodes: a dictionary object to lookup intersection nodes by label
     env: Underlying environment object for the node map
     """
     DG: nx.DiGraph = nx.DiGraph()
+    nodes: dict = None
     env: Env = None
 
     def from_yaml(self, path: str) -> None:
+        """
+        Initialise the Node Map from a yaml file
+        """
         with open(path, 'r') as file:
             things = yaml.safe_load(file)
 
@@ -56,6 +61,7 @@ class NodeMap(ABC):
                 uturnNode=node.get("uturn"),
             )
 
+        self.nodes = nodes  # dict initialisation
         self.DG.add_nodes_from([n for (_, n) in nodes.items()]) # just add the intersectionNode objects
 
         for node in nodes:
@@ -72,9 +78,11 @@ class NodeMap(ABC):
             if u.uturnNode is not None:
                 self.DG.add_edge(u, nodes[u.uturnNode])
 
-    def constructINM(self):
-        # Can probably hardcode this with specific coordinates
-        raise NotImplementedError
+    def path_from_node_labels(self, src: str, tgt: str) -> list[tuple[float]]:  
+        """
+        Shortest path between nodes. Returns a list of (x,y) tuples
+        """
+        return [(n.xPos, n.yPos) for n in nx.dijkstra_path(self.DG, self.nodes[src], self.nodes[tgt])]
 
     def displayINM(self,env):
         # Display INM over environment
@@ -84,9 +92,7 @@ class NodeMap(ABC):
 def main():
     nm = NodeMap()
     nm.from_yaml('PathPlanning/IntersectionNodeMap/nodemap.yaml')
-    print(nm.DG)
-    print(list(nm.DG.nodes))
-    print(list(nm.DG.edges))
+    print(nm.path_from_node_labels('A1', 'A2'))
 
 if __name__ == '__main__':
     main()
