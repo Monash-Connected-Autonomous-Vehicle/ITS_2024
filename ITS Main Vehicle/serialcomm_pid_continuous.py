5fdfd9c77f6b91f806af7abb719534d91556978b
GuanyueL <166686775+GuanyueL@users.noreply.github.com>
2024-09-06 18:30:50 +1000

    Add files via upload

---
+++ ITS Main Vehicle/serialcomm_pid_continuous.py
@@ -0,0 +1,33 @@
+import serial.tools.list_ports
+import time
+import pygame
+ports = serial.tools.list_ports.comports()
+pygame.display.init()
+# enter on terminal: python3 serialcomm_pid_final.py
+ser = serial.Serial('/dev/ttyUSB0',9600, timeout=1.0)
+time.sleep(0.003)
+q = 1
+screen = pygame.display.set_mode((800,600))
+WHITE = (255,255,255)
+screen.fill(WHITE)
+pygame.display.set_caption("vehicle control")
+while q == 1:
+    for event in pygame.event.get():
+    
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            ser.write(b'FORWARD\n')
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            ser.write(b'BACKWARD\n')
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            ser.write(b'LEFT\n')
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            ser.write(b'RIGHT\n')
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            ser.write(b'STOP\n')
+        elif event.type == pygame.KEYUP:
+            ser.write(b'STOP\n')
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            q == 0
+
+    time.sleep(0.003)
+ser.close()
